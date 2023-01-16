21f64b43167e9adc96eda2635d0cd9797c145d60
Kevin Schultz <ksschultz02@gmail.com>
2023-01-15 19:25:52 -0800

    Blender Image Viewer

---
+++ js/viewer.js
@@ -0,0 +1,13 @@
+var images = ["0.png", "1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png", "11.png", "12.png", "13.gif", "14.png", "15.png"]
+
+var current_image_index = 0;
+
+function previousImage() {
+    current_image_index = (current_image_index == 0) ? images.length - 1 : current_image_index - 1;
+    document.getElementById("blenderImage").src = "images/" + images[current_image_index];
+}
+
+function nextImage(){
+    current_image_index = (current_image_index == images.length - 1) ? 0: current_image_index + 1;
+    document.getElementById("blenderImage").src = "images/" + images[current_image_index];
+}
